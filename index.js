84aa3d902acbcf5b7d6896267080e3f832f6a494
Brian Ashton <ashtobd@gmail.com>
2023-07-06 07:47:14 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+  const letters = word.split("")
+  const reverseLetters = letters.reverse()
+  const combineLetters = reverseLetters.join("")
+
+  if (word === combineLetters) {
+    return true
+  }
+  else {
+    return false
+  }
+} 
 
 /* 
   Add your pseudocode here
+  isPalindrome function should split the word in half, reverse the two halves, then combine to see if it
+  matches the original word.
 */
 
 /*
   Add written explanation of your solution here
+  isPalindrome function takes word and uses .split; then it reverses the two halves using .reverse; it then
+  joins to two reversed halves together.
 */
 
 // You can run `node index.js` to view these console logs
